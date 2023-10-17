3fadf88ae35c0f35102c55e8e8135e06774ef78a
Tolrom <max.le.morlot@gmail.com>
2023-10-17 17:01:40 +0200

    PyDéfis + Exos JS

--- 5_Exercices_En_Cours_Formation/Algo/Javascript/script.js
+++ 5_Exercices_En_Cours_Formation/Algo/Javascript/script.js
@@ -97,13 +97,9 @@ cat();
 // Exercice 6
 
 function alpha(mots){
-    mots = prompt('Rentre trois mots séparés par un espace');
-    mots = mots.split(' ');
-    let tri = mots;
-    tri = tri.sort();
-    console.log(tri);
-    console.log(mots);
-    if (tri == mots){
+    mots = prompt('Rentre trois mots séparés par une virgule');
+    mots = mots.split(',');
+    if (mots[0] < mots[1] && mots[1] < mots[2]){
         console.log("Ils sont dans l'ordre alphabétique!");
     } 
     else {
@@ -111,4 +107,56 @@ function alpha(mots){
     }
 }
 
-alpha();
+// alpha();
+
+// Exercice 7
+
+function croissant(){
+    let tab = [];
+    tab.push(prompt('Entre un nombre'));
+    tab.push(prompt('Un autre'));
+    tab.push(prompt('Puis un dernier'));
+    if(tab[0] < tab[1] && tab[1] < tab[2]){
+        console.log('Les valeurs sont triées par ordre croissant');
+    }
+    else {
+        console.log('Les valeurs ne sont pas triées dans l\'ordre croissant');
+    }
+}
+
+// croissant();
+
+// Exercice 8
+
+function ttc(){
+    let tab = [];
+    tab.push(prompt('Entre le nom du produit'));
+    tab.push(parseFloat(prompt('Entre le prix HT')));
+    tab.push(parseInt(prompt('Saisis la quantité')));
+    tab.push(tab[1]*1.20*tab[2]);
+    console.log(tab[tab.length-1]);
+}
+
+// ttc();
+
+// Exercice 9
+
+let users = [];
+for(let i = 0 ; i < 5 ; i++){
+    let user = {
+        'nom': prompt(`Saisir le nom du ${(i+1)}e utilisateur`),
+        'prénom': prompt(`Saisir le prénom du ${(i+1)}e utilisateur`),
+        'âge': parseInt(prompt(`Saisir l'âge du ${(i+1)}e utilisateur`)),
+        'tel': parseInt(prompt(`Saisir le numéro de téléphone du ${(i+1)}e utilisateur`))
+    };
+    users.push(user);
+}
+let min = users[0]['âge'];
+let minInd;
+for(let i = 0 ; i < users.length ; i++){
+    if(users[i]['âge'] < min){
+        min = users[i]['âge'];
+        minInd = i;
+    }
+}
+console.log(`La personne la plus jeune s'appelle ${users[minInd]['prénom'] + ' ' + users[minInd]['nom']}`);
